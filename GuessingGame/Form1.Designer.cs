c466e95194eac8f7b8714a6ff0622ac48dd76756
dhruvdev <dhruva.oshea@connect.qut.edu.au>
2016-04-22 12:42:26 +1000

    Added Button and other labels

--- GuessingGame/Form1.Designer.cs
+++ GuessingGame/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace GuessingGame {
-    partial class Form1 {
+    partial class GuessingGameForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -25,6 +25,12 @@
         private void InitializeComponent() {
             this.SelectNumberLabel = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.ClickToSeeButton = new System.Windows.Forms.Button();
+            this.SorryIwasThinkingLabel = new System.Windows.Forms.Label();
+            this.CorrectGuessesLabel = new System.Windows.Forms.Label();
+            this.IncorrectGuessesLabel = new System.Windows.Forms.Label();
+            this.CorrectGuessesTextBox = new System.Windows.Forms.TextBox();
+            this.IncorrectGuessesTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -33,7 +39,7 @@
             this.SelectNumberLabel.AutoSize = true;
             this.SelectNumberLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.SelectNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.SelectNumberLabel.Location = new System.Drawing.Point(251, 94);
+            this.SelectNumberLabel.Location = new System.Drawing.Point(251, 80);
             this.SelectNumberLabel.Name = "SelectNumberLabel";
             this.SelectNumberLabel.Size = new System.Drawing.Size(142, 22);
             this.SelectNumberLabel.TabIndex = 0;
@@ -42,7 +48,7 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(420, 94);
+            this.numericUpDown1.Location = new System.Drawing.Point(420, 79);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             10,
             0,
@@ -63,15 +69,79 @@
             0,
             0});
             // 
-            // Form1
+            // ClickToSeeButton
+            // 
+            this.ClickToSeeButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClickToSeeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.ClickToSeeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickToSeeButton.Location = new System.Drawing.Point(295, 133);
+            this.ClickToSeeButton.Name = "ClickToSeeButton";
+            this.ClickToSeeButton.Size = new System.Drawing.Size(176, 79);
+            this.ClickToSeeButton.TabIndex = 2;
+            this.ClickToSeeButton.Text = "Click to see if you have guessed my number";
+            this.ClickToSeeButton.UseVisualStyleBackColor = false;
+            // 
+            // SorryIwasThinkingLabel
+            // 
+            this.SorryIwasThinkingLabel.AutoSize = true;
+            this.SorryIwasThinkingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SorryIwasThinkingLabel.Location = new System.Drawing.Point(247, 246);
+            this.SorryIwasThinkingLabel.Name = "SorryIwasThinkingLabel";
+            this.SorryIwasThinkingLabel.Size = new System.Drawing.Size(163, 20);
+            this.SorryIwasThinkingLabel.TabIndex = 3;
+            this.SorryIwasThinkingLabel.Text = "Sorry I was think of";
+            // 
+            // CorrectGuessesLabel
+            // 
+            this.CorrectGuessesLabel.AutoSize = true;
+            this.CorrectGuessesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CorrectGuessesLabel.Location = new System.Drawing.Point(180, 290);
+            this.CorrectGuessesLabel.Name = "CorrectGuessesLabel";
+            this.CorrectGuessesLabel.Size = new System.Drawing.Size(144, 20);
+            this.CorrectGuessesLabel.TabIndex = 4;
+            this.CorrectGuessesLabel.Text = "Correct Guesses";
+            // 
+            // IncorrectGuessesLabel
+            // 
+            this.IncorrectGuessesLabel.AutoSize = true;
+            this.IncorrectGuessesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IncorrectGuessesLabel.Location = new System.Drawing.Point(416, 290);
+            this.IncorrectGuessesLabel.Name = "IncorrectGuessesLabel";
+            this.IncorrectGuessesLabel.Size = new System.Drawing.Size(151, 20);
+            this.IncorrectGuessesLabel.TabIndex = 5;
+            this.IncorrectGuessesLabel.Text = "Incorect Guesses";
+            // 
+            // CorrectGuessesTextBox
+            // 
+            this.CorrectGuessesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CorrectGuessesTextBox.Location = new System.Drawing.Point(330, 290);
+            this.CorrectGuessesTextBox.Name = "CorrectGuessesTextBox";
+            this.CorrectGuessesTextBox.Size = new System.Drawing.Size(27, 23);
+            this.CorrectGuessesTextBox.TabIndex = 6;
+            // 
+            // IncorrectGuessesTextBox
+            // 
+            this.IncorrectGuessesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IncorrectGuessesTextBox.Location = new System.Drawing.Point(573, 290);
+            this.IncorrectGuessesTextBox.Name = "IncorrectGuessesTextBox";
+            this.IncorrectGuessesTextBox.Size = new System.Drawing.Size(26, 23);
+            this.IncorrectGuessesTextBox.TabIndex = 7;
+            // 
+            // GuessingGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(945, 467);
+            this.Controls.Add(this.IncorrectGuessesTextBox);
+            this.Controls.Add(this.CorrectGuessesTextBox);
+            this.Controls.Add(this.IncorrectGuessesLabel);
+            this.Controls.Add(this.CorrectGuessesLabel);
+            this.Controls.Add(this.SorryIwasThinkingLabel);
+            this.Controls.Add(this.ClickToSeeButton);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.SelectNumberLabel);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Name = "Form1";
+            this.Name = "GuessingGameForm";
             this.Text = "GuessingGameForm";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
@@ -83,6 +153,12 @@
 
         private System.Windows.Forms.Label SelectNumberLabel;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button ClickToSeeButton;
+        private System.Windows.Forms.Label SorryIwasThinkingLabel;
+        private System.Windows.Forms.Label CorrectGuessesLabel;
+        private System.Windows.Forms.Label IncorrectGuessesLabel;
+        private System.Windows.Forms.TextBox CorrectGuessesTextBox;
+        private System.Windows.Forms.TextBox IncorrectGuessesTextBox;
     }
 }
 
--- GuessingGame/Form1.cs
+++ GuessingGame/Form1.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuessingGame {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class GuessingGameForm : Form {
+        public GuessingGameForm() {
             InitializeComponent();
         }
     }
